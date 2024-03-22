0cc1a76c8f073a0e17fe1e9fed6ba932d4154e39
Varadhan <you@example.compsanbu0@gmail.com>
2024-03-22 22:16:32 +0530

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Homepage from "./pages/homepage.jsx";
+import About from "./pages/about.jsx";
+import Contact from "./pages/contact.jsx";
+import Projects from "./pages/projects.jsx";
+import Articles from "./pages/articles";
+import ReadArticle from "./pages/readArticle";
+import Notfound from "./pages/404";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/articles" element={<Articles />} />
+				<Route path="/article/:slug" element={<ReadArticle />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </Router>
   );
 }
+
+// function HomePages() {
+//   return <div>About Page</div>;
+// }
+
+// function AboutPage() {
+//   return <div>About Page</div>;
+// }
+
+// function ContactPage() {
+//   return <div>Contact Page</div>;
+// }
 
 export default App;
